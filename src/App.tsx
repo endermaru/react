7ae7a10ab513cbce85c4b27493f6ba9ae7b39c9e
endermaru <endermaru@snu.ac.kr>
2024-04-20 18:04:38 +0900

    step1

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect,useState} from 'react';
+import { JsxElement } from 'typescript';
 import './App.css';
 
 function App() {
+  
+  //Todo를 담는 리스트
+  type Todo = {id:number,title:string,memo:string};
+  type TodoList = Todo[];
+  const [todoList, setTodoList]=useState<TodoList>([
+    {id:0,title:'todo1',memo:'none'},
+    {id:1,title:'todo2',memo:'none'}
+  ]);
+  //현재 선택한 Id
+  const [id,setId] = useState(0);
+  //다음 할당할 Id
+  const [nextId, setNextId] = useState(todoList.length);
+  //Todolist 화면 모드
+  const [mode,setMode]=useState('MAIN'); //MAIN / READ / UPDATE
+
+  let content=null;
+  if (mode==='MAIN'){
+    
+  } else if (mode==='READ'){
+    
+  } else if (mode==='UPDATE'){
+    
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1><a href='/' onClick={(event)=>{
+        event.preventDefault();
+        setMode('MAIN');
+        setId(0);
+      }}>TodoList</a></h1>
+      {content}
     </div>
   );
 }
